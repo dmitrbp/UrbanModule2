fcb6e36c5d0b317c48da1c4a7676770caa0a67b8
dmitrbp <dmitrbp@jac.ru>
2024-06-11 13:32:04 +0300

    release 0

--- Module_2_5.py
+++ Module_2_5.py
@@ -8,7 +8,7 @@ def get_matrix(n, m, value):
     return matrix
 
 
-result0 = get_matrix(0, 0, 5)
+result0 = get_matrix(0, -1, 5)
 result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
 result3 = get_matrix(4, 2, 13)
